9bf81cedbf067955117b35717083b8653e6a580e
vasshorin <)tSz%gBWz1>
2023-01-07 21:29:36 -0800

    Add custom cetegories and accounts

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 // server.js
 
-const express = require('express');
-const mongoose = require('mongoose');
-const mongoURI = "mongodb+srv://testUser:testUser@cluster0.etygx.mongodb.net/new?retryWrites=true&w=majority";
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const { default: axios } = require('axios');
+const express = require("express");
+const mongoose = require("mongoose");
+const mongoURI =
+  "mongodb+srv://testUser:testUser@cluster0.etygx.mongodb.net/new?retryWrites=true&w=majority";
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const { default: axios } = require("axios");
 
 const app = express();
 
@@ -14,33 +15,37 @@ app.use(bodyParser.json());
 
 // Fix headers that are sent to the client after they are sent
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
-    res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
-    res.header('Access-Control-Allow-Credentials', true); // If needed
-    next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  ); // If needed
+  res.header("Access-Control-Allow-Headers", "X-Requested-With,content-type"); // If needed
+  res.header("Access-Control-Allow-Credentials", true); // If needed
+  next();
 });
-
 
 app.use(bodyParser.json());
 // Placeholder %PUBLIC_URL% for the public folder
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
 // Connect to the MongoDB database
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+mongoose
+  .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
     console.log("Connected to database!");
-});
+  });
 
 // Define the user schema
 const userSchema = new mongoose.Schema({
   email: String,
   password: String,
   name: String,
-  expenses: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Expense' }]
+  expenses: [{ type: mongoose.Schema.Types.ObjectId, ref: "Expense" }],
 });
 
 // Create the User model
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 // Define the expense schema
 const expenseSchema = new mongoose.Schema({
@@ -53,19 +58,35 @@ const expenseSchema = new mongoose.Schema({
   category: String,
 });
 
-app.get('/index.html', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+// Define the category schema
+const categorySchema = new mongoose.Schema({
+  category: String,
+});
+
+// Create the Category model
+const Category = mongoose.model("Category", categorySchema);
+
+// Define the account schema
+const accountSchema = new mongoose.Schema({
+  account: String,
+});
+
+// Create the Account model
+const Account = mongoose.model("Account", accountSchema);
+
+app.get("/index.html", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
 // Create the Expense model
-const Expense = mongoose.model('Expense', expenseSchema);
+const Expense = mongoose.model("Expense", expenseSchema);
 
 // Create a new user
-app.post('/users', (req, res) => {
+app.post("/users", (req, res) => {
   const user = new User({
     email: req.body.email,
     password: req.body.password,
-    name: req.body.name
+    name: req.body.name,
   });
 
   user.save((error) => {
@@ -78,7 +99,7 @@ app.post('/users', (req, res) => {
 });
 
 // Create a new expense
-app.post('/newExpenses', (req, res) => {
+app.post("/newExpenses", (req, res) => {
   const expense = new Expense({
     date: req.body.date,
     comment: req.body.comment,
@@ -86,7 +107,7 @@ app.post('/newExpenses', (req, res) => {
     importance: req.body.importance,
     account: req.body.account,
     category: req.body.category,
-    user: req.body.userId
+    user: req.body.userId,
   });
 
   expense.save((error) => {
@@ -98,8 +119,60 @@ app.post('/newExpenses', (req, res) => {
   });
 });
 
+// Add new category to category list
+app.post("/newCategory", (req, res) => {
+  const category = new Category({
+    category: req.body.category,
+  });
+
+  category.save((error) => {
+    if (error) {
+      res.status(500).send(error);
+    } else {
+      res.send(category);
+    }
+  });
+});
+
+// Get all categories
+app.get("/categories", (req, res) => {
+  Category.find((error, categories) => {
+    if (error) {
+      res.status(500).send(error);
+    } else {
+      res.send(categories);
+    }
+  });
+});
+
+// Add new account to account list
+app.post("/newAccount", (req, res) => {
+  const account = new Account({
+    account: req.body.account,
+  });
+
+  account.save((error) => {
+    if (error) {
+      res.status(500).send(error);
+    } else {
+      res.send(account);
+    }
+  });
+});
+
+// Get all accounts
+app.get("/accounts", (req, res) => {
+  Account.find((error, accounts) => {
+    if (error) {
+      res.status(500).send(error);
+    } else {
+      res.send(accounts);
+    }
+  });
+});
+
 // Get a list of expenses for a user
-app.get('/users/:userId/expenses', (req, res) => {
+app.get("/users/:userId/expenses", (req, res) => {
   Expense.find({ user: req.params.userId }, (error, expenses) => {
     if (error) {
       res.status(500).send(error);
@@ -110,11 +183,10 @@ app.get('/users/:userId/expenses', (req, res) => {
 });
 
 // Get all expenses
-app.get('/expenses', (req, res) => {
+app.get("/expenses", (req, res) => {
   Expense.find((error, expenses) => {
     if (error) {
-      res.status
-      (500).send(error);
+      res.status(500).send(error);
     } else {
       res.send(expenses);
     }
@@ -122,11 +194,10 @@ app.get('/expenses', (req, res) => {
 });
 
 // delete expense
-app.post('/expenses/:id', (req, res) => {
+app.post("/expenses/:id", (req, res) => {
   Expense.findByIdAndDelete(req.params.id, (error, expense) => {
     if (error) {
-      res.status(500).send
-      (error);
+      res.status(500).send(error);
     } else {
       res.send(expense);
     }
@@ -135,6 +206,5 @@ app.post('/expenses/:id', (req, res) => {
 
 const PORT = process.env.PORT || 3004;
 app.listen(PORT, () => {
-    console.log(`Our app is running on port htpp://localhost:${ PORT }`);
+  console.log(`Our app is running on port htpp://localhost:${PORT}`);
 });
-
--- src/App.js
+++ src/App.js
@@ -14,6 +14,10 @@ const App = () => {
   const [userId, setUserId] = useState("");
   const [expenses, setExpenses] = useState([]);
   const [id, setId] = useState("");
+  const [newOption, setNewOption] = useState("");
+  const [categories, setCategories] = useState([]);
+  const [accounts, setAccounts] = useState([]);
+  const [newAccount, setNewAccount] = useState("");
 
   // Handle form submission
   const handleSubmit = (event) => {
@@ -50,12 +54,34 @@ const App = () => {
       });
   };
 
+  //  Let the user add a new option to the select elemen
+  const handleSubmit1 = (event) => {
+    event.preventDefault();
+    setNewOption("");
+  };
+
+  // Handle form input changes
+  const handleChange = (event) => {
+    setNewOption(event.target.value);
+  };
+
+  const handleChange1 = (event) => {
+    setNewAccount(event.target.value);
+  };
+
+  // Handle form input changes
+  const handleDateChange = (event) => {
+    setDate(event.target.value);
+  };
+
+
+
+
   // Fetch the expenses from the server when the component mounts
   useEffect(() => {
     axiosConfig
       .get("/expenses")
       .then((response) => {
-        //  setExpenses(response.data);
         setExpenses([
           ...response.data,
           {
@@ -74,6 +100,63 @@ const App = () => {
       });
   }, []);
 
+  // Fetch the categories from the server when the component mounts
+  useEffect(() => {
+    axiosConfig
+      .get("/categories")
+      .then((response) => {
+        setCategories(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+// Add new category
+  const addCategory = (event) => {
+    event.preventDefault();
+    axiosConfig
+      .post("/newCategory", {
+        category: newOption,
+      })
+      .then((response) => {
+        console.log(response.data);
+        setCategories([...categories, response.data]);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  // Add new account
+  const addAccount = (event) => {
+    event.preventDefault();
+    axiosConfig
+      .post("/newAccount", {
+        account: newAccount,
+      })
+      .then((response) => {
+        console.log(response.data);
+        setAccounts([...accounts, response.data]);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  // Fetch all accounts from the server when the component mounts
+  useEffect(() => {
+    axiosConfig
+      .get("/accounts")
+      .then((response) => {
+        setAccounts(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+
   // Delete an expense from the server and update the list of expenses in the state when the user clicks the delete button for an expense
   const handleDelete = (id) => {
     axiosConfig.post(`/expenses/${id}`).then((response) => {
@@ -81,14 +164,14 @@ const App = () => {
       setExpenses(newExpenses);
     });
   };
-
+  // Filter expenses by importance 
   const filterExpenses = (event) => {
     const filteredExpenses = expenses.filter(
       (expense) => expense.importance === event.target.value
     );
     setExpenses(filteredExpenses);
   };
-
+  // Filter expenses by account
   const filterBYCategory = (event) => {
     const filteredExpenses = expenses.filter(
       (expense) => expense.category === event.target.value
@@ -100,6 +183,33 @@ const App = () => {
   return (
     <div>
       <h1>Expense Tracker</h1>
+      {/* Add new category */}
+      <h2>Add new category</h2>
+      <form onSubmit={handleSubmit1}>
+        <label htmlFor="newOption">New Category:</label>
+        <input
+          type="text"
+          id="newOption"
+          value={newOption}
+          onChange={handleChange}
+        />
+        <button type="submit" onClick={addCategory}> Add Category</button>
+      </form>
+
+{/* Add new account */}
+      <h2>Add new account</h2>
+      <form onSubmit={handleSubmit1}>
+        <label htmlFor="newAccount">New Account:</label>
+        <input
+          type="text"
+          id="newAccount"
+          value={newAccount}
+          onChange={handleChange1}
+        />
+        <button type="submit" onClick={addAccount}> Add Account</button>
+      </form>
+
+
       <h2>Add Expense</h2>
       <form onSubmit={handleSubmit}>
         <label htmlFor="date">Date:</label>
@@ -142,12 +252,21 @@ const App = () => {
         </select>
         <br />
         <label htmlFor="account">Account:</label>
-        <input
-          type="text"
+        <select
           id="account"
           value={account}
+          required={true}
           onChange={(event) => setAccount(event.target.value)}
-        />
+        >
+          {/* Make a default value first  */}
+          <option value="">Select an account</option>
+          {accounts.map((account) => (
+            <option key={account._id} value={account.account}>
+              {account.account}
+            </option>
+          ))}
+        </select>
+  
         <br />
         <label htmlFor="category">Category:</label>
         <select
@@ -158,10 +277,12 @@ const App = () => {
         >
           {/* Make a default value first  */}
           <option value="">Select a category</option>
-          <option value="rent">Rent</option>
-          <option value="grocery">Grocery</option>
-          <option value="dining">Dining</option>
-          <option value="fees">Fees</option>
+          {categories.map((category) => (
+            <option key={category._id} value={category.category}>
+              {category.category}
+            </option>
+          ))}
+        
         </select>
         <br />
         <br />
@@ -187,7 +308,6 @@ const App = () => {
 
       <select onChange={filterBYCategory}>
         <input
-
           type="text"
           value={category}
           onChange={(event) => setCategory(event.target.value)}
@@ -198,7 +318,7 @@ const App = () => {
         <option value="dining">Dining</option>
         <option value="fees">Fees</option>
       </select>
-      
+
       {/* Display the expenses */}
       <h2>Expenses</h2>
       <table>
